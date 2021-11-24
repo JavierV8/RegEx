474bbdcca3ddfb0338c3487abbafc319bba6f767
Javier Sanchez <javier.san.mail@gmail.com>
2021-11-24 20:27:54 +0100

    created first regex

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result1, setResult1] = useState(null);
+  const text1 = 'A regular expression (shortened as regex or regexp also referred to as rational expression) is a sequence of characters that specifies a search pattern. Usually such patterns are used by string-searching algorithms for "find" or "find and replace" operations on strings, or for input validation. It is a technique developed in theoretical computer science and formal language theory.';
+
+
+  useEffect(() => {
+    console.log(text1.replace(/[^a-zA-Z]+/g, " ").split(" ").filter(word => word.match(/on\b/g)))
+    setResult1(text1.replace(/[^a-zA-Z]+/g, " ").split(" ").filter(word => word.match(/on\b/g)));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p>match all words that ends with "on"</p>
+        <p>text1.replace(/[^a-zA-Z]+/g, " ").split(" ").filter(word => word.match(/on\b/g))</p>
+        <h3>Text</h3>
+        <p>{text1}</p>
+
+        <h3>Result</h3>
+        <p>{result1}</p>
+      </div>
     </div>
   );
 }
