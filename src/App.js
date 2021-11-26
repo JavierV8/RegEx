f17e28801c4c1fec7ac1f2f20f675a6611fd1033
Javier Sanchez <javier.san.mail@gmail.com>
2021-11-26 17:19:07 +0100

    created new regex example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Component1 from './components/compoent1';
+import Component2 from './components/component2';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Component1 />
+      <Component2 />
     </div>
   );
 }
---
+++ src/components/component2.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import "../App.css";
+
+function App() {
+  const [result1, setResult1] = useState(null);
+  const text1 = 'A regular expression (shortened as regex or regexp also referred to as rational expression) is a sequence of characters that specifies a search pattern. Usually such patterns are used by string-searching algorithms for "find" or "find and replace" operations on strings, or for input validation. It is a technique developed in theoretical computer science and formal language theory.';
+
+
+  useEffect(() => {
+      setResult1(text1.replace(/[^a-zA-Z]+/g, " ").split(" ").filter(word => word.match(/\bse/g)));
+  }, []);
+  return (
+    <div className="App">
+      <div className="box">
+        <p>match all words that start with "se"</p>
+        <p>text1.replace(/[^a-zA-Z]+/g, " ").split(" ").filter(word => word.match(/\bse/g))</p>
+        <h3>Text</h3>
+        <p>{text1}</p>
+
+        <h3>Result</h3>
+        <p>{result1}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
